e2a5045ae2332d762ca3e35e122624b5464d8ad3
alexeypato <alexeypato@gmail.com>
2017-01-02 23:47:41 +0300

    master. First build to Heroku

---
+++ src/components/CashInfoTable.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+function priceFormatter(cell, row){
+  return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+}
+
+export default class CashInfoTable extends React.Component {
+  render() {
+
+    /*const options = {
+      page: 0,  // which page you want to show as default
+      sizePerPageList: [ {
+        text: '5', value: 5
+      }, {
+        text: '10', value: 10
+      }, {
+        text: 'All', value: this.props.accountHistory.length
+      } ], // you can change the dropdown list for size per page
+      sizePerPage: 5,  // which size per page you want to locate as default
+      pageStartIndex: 0, // where to start counting the pages
+      paginationSize: 3,  // the pagination bar size.
+      prePage: 'Prev', // Previous page button text
+      nextPage: 'Next', // Next page button text
+      firstPage: 'First', // First page button text
+      lastPage: 'Last', // Last page button text
+      paginationShowsTotal: true,  // Accept bool or function
+      hideSizePerPage: true //> You can hide the dropdown for sizePerPage
+    };*/
+
+    const options = {
+      sizePerPage: 5,  // which size per page you want to locate as default
+      paginationSize: 3,  // the pagination bar size.
+      prePage: 'Prev', // Previous page button text
+      nextPage: 'Next', // Next page button text
+      hideSizePerPage: true
+    };
+
+    return (
+      <div><h4>History of receipt</h4>
+        <BootstrapTable
+          data={ this.props.accountHistory }
+          striped
+          pagination={ true }
+          options={ options }>
+          <TableHeaderColumn dataField='id' isKey={true} dataAlign="center" dataSort={true}>ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='cash' dataFormat={priceFormatter}>Sum of money</TableHeaderColumn>
+          <TableHeaderColumn dataField='date' dataSort={true}>Date of receipt</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
---
+++ src/inputs/Input.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../assets/stylesheets/input.scss';
+
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      textError: '',
+    }
+  }
+
+  handleOnChangeInput = () => {
+    this.setState({
+      value: ReactDOM.findDOMNode(this.refs.cash).value.replace(/\D/, '')
+    })
+  };
+
+  handleAddCash = () => {
+    if (this.state.value.length > 0) {
+      if (this.state.value.replace(/\d/g, '').length)
+      {
+        this.setState({ value: '' });
+        this.setState({ textError: 'Error! Invalid sum of money.' });
+      } else {
+        this.props.addCash(this.state.value);
+        this.setState({ value: '' });
+        this.setState({ textError: '' });
+      }
+    } else{
+      this.setState({ textError: 'Error! Enter sum of money.' });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={() => this.handleOnChangeInput()}
+          placeholder='Sum of money'
+          value={this.state.value}
+          ref='cash'
+        />
+        <a
+          className='add-btn'
+          onClick={() => this.handleAddCash()}
+          href={undefined}
+          >
+          Place
+        </a>
+        <p>{this.state.textError}</p>
+      </div>
+    );
+  }
+};
