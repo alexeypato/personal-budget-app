85e1b5716d26b1ae9d50f37976709692b4246c26
alexeypato <alexeypato@gmail.com>
2017-01-08 12:07:33 +0300

    bugInsertTable. Add react-bootstrap-modal

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import './assets/stylesheets/main.scss';
 require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap/dist/css/bootstrap-theme.min.css');
 require('react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
+require('react-bootstrap-modal/lib/styles/rbm-patch.css');
 // require('react-bootstrap/dist/react-bootstrap.min.js');
 // require('react-bootstrap-table/dist/react-bootstrap-table.min.js');
 
--- src/inputs/Input.jsx
+++ src/inputs/Input.jsx
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 import CustomControl from '../components/CustomControl';
 
 const DatePicker = require('react-bootstrap-date-picker');
+const Modal = require('react-bootstrap-modal');
 
 export default class Input extends React.Component {
   constructor(props) {
@@ -22,6 +23,30 @@ export default class Input extends React.Component {
   };
 
   handleAddCash = () => {
+    this.setState({ open: true });
+    /* if (this.state.value.length > 0) {
+      if (this.state.value.replace(/\d/g, '').length) {
+        this.setState({ value: '' });
+        this.setState({ textError: 'Error! Invalid sum of money.' });
+      } else {
+        this.props.addCash(this.state.value, this.state.date);
+        this.setState({ value: '' });
+        this.setState({ textError: '' });
+      }
+    } else {
+      this.setState({ textError: 'Error! Enter sum of money.' });
+    }*/
+  };
+
+  handleOnChangeDate = (value) => {
+    this.setState({
+      date: value,
+    });
+  };
+
+  closeModal = () => this.setState({ open: false })
+
+  saveAndClose = () => {
     if (this.state.value.length > 0) {
       if (this.state.value.replace(/\d/g, '').length) {
         this.setState({ value: '' });
@@ -34,13 +59,9 @@ export default class Input extends React.Component {
     } else {
       this.setState({ textError: 'Error! Enter sum of money.' });
     }
-  };
-
-  handleOnChangeDate = (value) => {
-    this.setState({
-      date: value,
-    });
-  };
+    // api.saveData()
+    // .then(() => this.setState({ open: false }));
+  }
 
   render() {
     return (
@@ -76,6 +97,50 @@ export default class Input extends React.Component {
             >
               Place
             </Button>
+            <Modal
+              show={this.state.open}
+              onHide={this.closeModal}
+              aria-labelledby="ModalHeader"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="ModalHeader">A Title Goes here</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-3 text-center padding-margin-none">
+                      <input
+                        className="form-control text-center"
+                        onChange={() => this.handleOnChangeInput()}
+                        placeholder={
+                          this.state.textError ?
+                            this.state.textError :
+                            'Sum of money'
+                        }
+                        value={this.state.value}
+                        maxLength="10"
+                        ref="cash"
+                      />
+                    </div>
+                    <div className="col-md-1 padding-margin-none">
+                      <DatePicker
+                        customControl={<CustomControl />}
+                        onChange={this.handleOnChangeDate}
+                        value={this.state.date}
+                        dateFormat="YYYY-MM-DD"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Modal.Dismiss className="btn btn-default">Cancel</Modal.Dismiss>
+
+                <button className="btn btn-primary" onClick={this.saveAndClose}>
+                  Save
+                </button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
       </div>
