836a2fa7554fa610a830c55e8539e9151a7dd363
alexeypato <alexeypato@gmail.com>
2017-04-18 16:02:55 +0300

    master. Add tests #3

--- src/containers/App.test.jsx
+++ src/containers/App.test.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import sinon from 'sinon';
+import renderer from 'react-test-renderer';
 import { App } from './App';
 import Footer from '../components/footer/Footer';
 import Header from '../components/Header';
 
 describe('App component', () => {
-  let wrapper;
   const props = {
-    auth: {},
-  }
-
-  beforeEach(() => {
-    wrapper = shallow(<App {...props} />);
-  });
+    auth: { 
+      authenticated: false,
+      displayName: null,
+      email: null,
+      id: null,
+    },
+  };
 
   it('should render <Header /> component', () => {
+    const wrapper = shallow(<App {...props} />);
     expect(wrapper.find(Header).length).toBe(0);
   });
 
   it('should render <Footer /> component', () => {
+    const wrapper = shallow(<App {...props} />);
     expect(wrapper.find(Footer).length).toBe(1);
+  });
+
+  it('calls componentWillReceiveProps', () => {
+    const spy = jest.spyOn(App.prototype, 'componentWillReceiveProps');
+    const wrapper = mount(<App {...props} />);
+    wrapper.instance().componentWillReceiveProps(props);
+    expect(spy).toHaveBeenCalled();
   });
 });
